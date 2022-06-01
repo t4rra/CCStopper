89cec7397c4619744833ac24ce9d6f275fa39d9f
Professional <shubhojeetde2@gmail.com>
2022-06-01 17:14:10 +0530

    Add option to hide trial banner

--- CCStopper.bat
+++ CCStopper.bat
@@ -35,21 +35,28 @@ echo                  ^|      [5] Hide Creative Cloud Files from File Explorer  
 echo                  ^|                                                               ^|
 echo                  ^|      [6] Disable Adobe Processes & Services Auto-Start        ^|
 echo                  ^|                                                               ^|
+echo                  ^|      [7] Hide Trial Banner                                    ^|
+echo                  ^|                                                               ^|
 echo                  ^|      ___________________________________________________      ^|
 echo                  ^|                                                               ^|
-echo                  ^|      [7] Github Repo (Detailed instructions there)            ^|
+echo                  ^|      [8] Github Repo (Detailed instructions there)            ^|
 echo                  ^|                                                               ^|
-echo                  ^|      [8] Exit                                                 ^|
+echo                  ^|      [9] Exit                                                 ^|
 echo                  ^|                                                               ^|
 echo                  ^|_______________________________________________________________^|
 echo:          
 choice /C:12345678 /N /M ">                                     Select [1,2,3,4,5,6,7,8]: "
 
 cls
-if errorlevel 8 exit
-if errorlevel 7 (
+if errorlevel 9 exit
+if errorlevel 8 (
 	cls
 	start https://github.com/eaaasun/CCStopper
+	goto menu
+)
+if errorlevel 7 (
+	:: Source: https://www.reddit.com/r/GenP/comments/qwermj/wrote_a_quick_script_to_fix_up_the_trial_banner/
+	Powershell -ExecutionPolicy RemoteSigned -File .\scripts\TrialRemove.ps1
 	goto menu
 )
 if errorlevel 6 (
---
+++ scripts/TrialRemove.ps1
@@ -0,0 +1,27 @@
+$StylePath = 'C:\Program Files\Common Files\Adobe\UXP\extensions\com.adobe.ccx.start-5.9.0\css\styles.css'
+$StylePath1 = 'C:\Program Files\Adobe\Adobe Photoshop 2022\Required\UXP\com.adobe.ccx.start\css\styles.css'
+$StylePath2 = 'C:\Program Files\Adobe\Adobe Illustrator 2022\Support Files\Required\UXP\extensions\com.adobe.ccx.start\css\styles.css'
+$LocalePath = 'C:\Program Files (x86)\Common Files\Adobe\AdobeGCClient\locales'
+
+$Style_None = '{"display":"none"}'
+$Style_TrialExpiresBanner = '{"background-color":"#1473E6"}'
+$Style_TrialEnded = '{"background-color":"#d7373f"}'
+
+# Back up file
+powershell -Command "(gc '$StylePath') | Out-File -encoding ASCII '$StylePath.bak'"
+
+# Replace contents
+
+(Get-Content $StylePath -Raw) -replace $Style_TrialExpiresBanner, $Style_None | Set-Content $StylePath
+(Get-Content $StylePath1 -Raw) -replace $Style_TrialExpiresBanner, $Style_None | Set-Content $StylePath1
+(Get-Content $StylePath2 -Raw) -replace $Style_TrialExpiresBanner, $Style_None | Set-Content $StylePath2
+(Get-Content $StylePath -Raw) -replace $Style_TrialEnded, $Style_None | Set-Content $StylePath
+(Get-Content $StylePath1 -Raw) -replace $Style_TrialEnded, $Style_None | Set-Content $StylePath1
+(Get-Content $StylePath2 -Raw) -replace $Style_TrialEnded, $Style_None | Set-Content $StylePath2
+
+# Delete Language Packs
+$ErrorActionPreference= 'silentlycontinue'
+Remove-Item '$LocalePath' -Force -Recurse
+
+$Shell = New-Object -ComObject "WScript.Shell"
+$Button = $Shell.Popup("Should be done!", 0, "Trial Banner Remover", 0)
