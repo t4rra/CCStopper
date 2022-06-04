d5eba8c390a973faeb231d17c334c81cc997f278
E-Soda <64815861+eaaasun@users.noreply.github.com>
2022-06-04 11:05:28 -0700

    add admin prompt/UI

--- scripts/patchhostfile.bat
+++ scripts/patchhostfile.bat
@@ -1,27 +1,15 @@
 @echo off
-
-echo Checking..
-
 setlocal EnableDelayedExpansion
+mode con: cols=100 lines=46
+
+:: Asks for Administrator Permissions
+%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd","/c %~s0 ::","","runas",1)(window.close) && exit
+cd /d "%~dp0"
 
 set LOCAL_ADDRESS=127.0.0.1
 set BLOCKED_ADDRESSES="ic.adobe.io" "52.6.155.20" "52.10.49.85" "23.22.30.141" "34.215.42.13" "52.84.156.37" "65.8.207.109" "3.220.11.113" "3.221.72.231" "3.216.32.253" "3.208.248.199" "3.219.243.226" "13.227.103.57" "34.192.151.90" "34.237.241.83" "44.240.189.42" "52.20.222.155" "52.208.86.132" "54.208.86.132" "63.140.38.120" "63.140.38.160" "63.140.38.169" "63.140.38.219" "wip.adobe.com" "adobeereg.com" "18.228.243.121" "18.230.164.221" "54.156.135.114" "54.221.228.134" "54.224.241.105" "100.24.211.130" "162.247.242.20" "wip1.adobe.com" "wip2.adobe.com" "wip3.adobe.com" "wip4.adobe.com" "3dns.adobe.com" "ereg.adobe.com" "199.232.114.137" "bam.nr-data.net" "practivate.adobe" "ood.opsource.net" "crl.verisign.net" "3dns-1.adobe.com" "3dns-2.adobe.com" "3dns-3.adobe.com" "3dns-4.adobe.com" "hl2rcv.adobe.com" "genuine.adobe.com" "www.adobeereg.com" "www.wip.adobe.com" "www.wip1.adobe.com" "www.wip2.adobe.com" "www.wip3.adobe.com" "www.wip4.adobe.com" "ereg.wip.adobe.com" "ereg.wip.adobe.com" "activate.adobe.com" "adobe-dns.adobe.com" "ereg.wip1.adobe.com" "ereg.wip2.adobe.com" "ereg.wip3.adobe.com" "ereg.wip4.adobe.com" "ereg.wip1.adobe.com" "ereg.wip2.adobe.com" "ereg.wip3.adobe.com" "ereg.wip4.adobe.com" "cc-api-data.adobe.io" "practivate.adobe.ntp" "practivate.adobe.ipp" "practivate.adobe.com" "adobe-dns-1.adobe.com" "adobe-dns-2.adobe.com" "adobe-dns-3.adobe.com" "adobe-dns-4.adobe.com" "lm.licenses.adobe.com" "hlrcv.stage.adobe.com" "prod.adobegenuine.com" "practivate.adobe.newoa" "activate.wip.adobe.com" "activate-sea.adobe.com" "uds.licenses.adobe.com" "k.sni.global.fastly.net" "activate-sjc0.adobe.com" "activate.wip1.adobe.com" "activate.wip2.adobe.com" "activate.wip3.adobe.com" "activate.wip4.adobe.com" "na1r.services.adobe.com" "lmlicenses.wip4.adobe.com" "na2m-pr.licenses.adobe.com" "wwis-dubc1-vip60.adobe.com" "workflow-ui-prod.licensingstack.com"
 set "HOST_FILE=%WINDIR%\system32\drivers\etc\hosts"
 
->NUL 2>&1 OPENFILES
-if not %errorlevel%==0 (
-    echo:
-    echo ERROR: Administrator not detected.
-    echo You need to 'Run as Administrator'^^!
-    echo:
-    echo Righ-Click and 'Run as Administrator'
-    echo:
-    echo:
-    goto :END
-)
-
-echo Administrator detected.
-echo:
 echo Hosts file : "!HOST_FILE!"
 echo:
 call :CHECK_FILE_ATTRIBUTES HOST_FILE || (
@@ -79,21 +67,43 @@ for %%A in (!BLOCKED_ADDRESSES!) do (
         goto :WRITING_FAILURE
     )
 )
+cls
 echo:
-echo Patching is completed.
-echo Check hosts file if you want to see the result.
+echo:
+echo                   _______________________________________________________________
+echo                  ^|                                                               ^| 
+echo                  ^|                                                               ^|
+echo                  ^|                            CCSTOPPER                          ^|
+echo                  ^|                         Made by eaaasun                       ^|
+echo                  ^|                       HostFilePatch Module                    ^|
+echo                  ^|      ___________________________________________________      ^|
+echo                  ^|                                                               ^|
+echo                  ^|                       Patching completed.                     ^|
+echo                  ^|      ___________________________________________________      ^|
+echo                  ^|                                                               ^|
+echo                  ^|      [1] Exit                                                 ^|
+echo                  ^|                                                               ^|
+echo                  ^|                                                               ^|
+echo                  ^|_______________________________________________________________^|
+echo:          
+choice /C:1 /N /M ">                                         Select [1]: "
+
+cls
+if errorlevel 1 (
+    goto END
+)
 echo:
 
 :END
-PAUSE
-ENDLOCAL
-exit /b 0
+start cmd /k %~dp0\..\CCStopper.bat
+exit
 
 :WRITING_FAILURE
 call :CHECK_FILE_ATTRIBUTES HOST_FILE && (
     echo ERROR: Something went wrong and the script could not write in your hosts file.
 )
-goto :END
+pause
+goto END
 
 :CHECK_FILE_ATTRIBUTES
 call :GET_FILE_ATTRIBUTES %1
