4ca3090e25c562c2b4536235c347ad7734d41461
E-Soda <64815861+eaaasun@users.noreply.github.com>
2022-06-03 20:31:39 -0700

    add option to unhide folder

--- scripts/HideCCFiles.bat
+++ scripts/HideCCFiles.bat
@@ -2,6 +2,10 @@
 title CCStopper - Hide Creative Cloud Files
 mode con: cols=100 lines=42
 
+:: Asks for Administrator Permissions
+%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd","/c %~s0 ::","","runas",1)(window.close) && exit
+cd /d "%~dp0"
+
 Setlocal EnableDelayedExpansion
 for /f "usebackq delims=" %%a in (`reg query "HKEY_CURRENT_USER\SOFTWARE\Classes\CLSID"`) do (
 	set key=%%a
@@ -14,13 +18,44 @@ Setlocal DisableDelayedExpansion
 :patchCheck
 for /f "usebackq tokens=3*" %%A IN (`reg query %clsid% /v System.IsPinnedToNameSpaceTree`) do set data=%%A %%B
 
+set folderHidden=false
+
 if %data% EQU 0 (
+	set folderHidden=true
 	cls
-	echo.
-	echo Creative Cloud Files are already hidden.
-	pause
-	exit
+	:: Thanks https://github.com/massgravel/Microsoft-Activation-Scripts for the UI
+	echo:
+	echo:
+	echo                   _______________________________________________________________
+	echo                  ^|                                                               ^| 
+	echo                  ^|                                                               ^|
+	echo                  ^|                            CCSTOPPER                          ^|
+	echo                  ^|                         Made by eaaasun                       ^|
+	echo                  ^|                        HideCCFiles Module                     ^|
+	echo                  ^|      ___________________________________________________      ^|
+	echo                  ^|                                                               ^|
+	echo                  ^|          CREATIVE CLOUD FILES FOLDER ALREADY HIDDEN!          ^|
+	echo                  ^|                                                               ^|
+	echo                  ^|      Would you like to restore the folder's visibility?       ^|
+	echo                  ^|      ___________________________________________________      ^|
+	echo                  ^|                                                               ^|
+	echo                  ^|      [1] Restore Creative Cloud Files folder                  ^|
+	echo                  ^|      ___________________________________________________      ^|
+	echo                  ^|                                                               ^|
+	echo                  ^|      [2] Exit                                                 ^|
+	echo                  ^|                                                               ^|
+	echo                  ^|                                                               ^|
+	echo                  ^|_______________________________________________________________^|
+	echo:          
+	choice /C:12 /N /M ">                                            Select [1,2]: "
+	if errorlevel 2 (
+		goto exit
+	)
+	if errorlevel 1 (
+		goto mainScript
+	)
 ) else (
+	@REM set folderHidden=false
 	goto mainScript
 )
 
@@ -29,10 +64,6 @@ start cmd /k %~dp0\..\CCStopper.bat
 exit
 
 :mainScript
-:: Asks for Administrator Permissions
-%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd","/c %~s0 ::","","runas",1)(window.close) && exit
-cd /d "%~dp0"
-
 cls
 :: Thanks https://github.com/massgravel/Microsoft-Activation-Scripts for the UI
 echo:
@@ -61,20 +92,37 @@ echo                  ^|                                                        
 echo                  ^|                                                               ^|
 echo                  ^|_______________________________________________________________^|
 echo:          
-choice /C:123 /N /M ">                                            Select [1,2,3]: "
+choice /C:123 /N /M ">                                         Select [1,2,3]: "
 
 cls
 if errorlevel 3 (
 	goto exit
 )
-if errorlevel 2 goto:editReg
+if errorlevel 2 (
+	if %folderHidden% == true (
+		goto showFolder
+	) else (
+		goto hideFolder
+	)
+)
 if errorlevel 1 (
 	echo Creating system restore point, please be patient.
 	wmic /Namespace:\\root\default Path SystemRestore Call CreateRestorePoint "Before CCStopper Hide CC Files Script", 100, 12
-	goto editReg
+	if %folderHidden% == true (
+		goto showFolder
+	) else (
+		goto hideFolder
+	)
 )
 
-:editReg
+
+:showFolder
+:: shows CCF in explorer
+reg add %clsid% /v System.IsPinnedToNameSpaceTree /t REG_DWORD /d 1 /f /reg:64
+goto restartAsk
+pause
+
+:hideFolder
 :: Hides CCF from file explorer
 reg add %clsid% /v System.IsPinnedToNameSpaceTree /t REG_DWORD /d 0 /f /reg:64
 goto restartAsk
@@ -95,12 +143,13 @@ echo                  ^|      __________________________________________________
 echo                  ^|                                                               ^|
 echo                  ^|                Hiding CCF from explorer complete!             ^|
 echo                  ^|                                                               ^|
-echo                  ^|      The system needs to restart for changes to apply.        ^|
+echo                  ^|      Windows Explorer needs to restart for changes to         ^|
+echo                  ^|      apply. Things will flash; please do not worry.           ^|
 echo                  ^|      ___________________________________________________      ^|
 echo                  ^|                                                               ^|
 echo                  ^|      [1] Restart now                                          ^|
 echo                  ^|                                                               ^|
-echo                  ^|      [2] Skip (You will need to manually restart later)       ^|
+echo                  ^|      [2] Skip (Please restart from task manager later)        ^|
 echo                  ^|                                                               ^| 
 echo                  ^|                                                               ^| 
 echo                  ^|_______________________________________________________________^|
@@ -112,5 +161,6 @@ if errorlevel 2 (
 )
 if errorlevel 1 (
 	cls
-	shutdown /r /t 0
+	taskkill /F /IM explorer.exe & start explorer
+	goto exit 
 )
