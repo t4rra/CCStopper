025eef4eda6ac278f1ba6f0bd524dbb9b287e5ef
E-Soda <64815861+eaaasun@users.noreply.github.com>
2022-06-15 11:11:20 -0700

    fix file path error

--- scripts/HideTrialBanner.ps1
+++ scripts/HideTrialBanner.ps1
@@ -14,15 +14,16 @@ $Host.UI.RawUI.WindowTitle = $MyInvocation.MyCommand.Definition + "(Elevated)"
 }
 
 function Get-UninstallKey ([String]$ID) {
-	return (Get-ChildItem HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall -Recurse | Where-Object {$_.PSChildName -Like "$ID_*"}).Name
+	return (Get-ChildItem HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall -Recurse | Where-Object {$_.PSChildName -Like "$ID*"}).Name
 }
 
+$AppLocation = (Get-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\CORE_1_0_32").InstallLocation
 $PSAppLocation = (Get-ItemProperty -Path Registry::$(Get-UninstallKey -ID "PHSP") -Name InstallLocation)
 $AIAppLocation = (Get-ItemProperty -Path Registry::$(Get-UninstallKey -ID "ILST") -Name InstallLocation)
-$Button = $Shell.Popup($AppLocation, 0, "Trial Banner Hider", 0)
+# $Button = $Shell.Popup($AppLocation, 0, "Trial Banner Hider", 0)
 
 $CommonExtensions = 'C:\Program Files\Common Files\Adobe\UXP\extensions'
-$StylePath = "$CommonExtensions\$((Get-ChildItem $CommonExtensions -Recurse | Where-Object {$_.PSChildName -Like 'com.adobe.ccx.start-*'}).Name)\css\styles.css"
+$StylePath = "$CommonExtensions\$((Get-ChildItem $CommonExtensions -Recurse | Where-Object {$_.PSChildName -Like 'com.adobe.ccx.start-*' } | Select -Last 1).Name)\css\styles.css"
 $StylePath1 = "$($PSAppLocation)\Required\UXP\com.adobe.ccx.start\css\styles.css"
 $StylePath2 = "$($AIAppLocation)\Support Files\Required\UXP\extensions\com.adobe.ccx.start\css\styles.css"
 $LocalePath = 'C:\Program Files (x86)\Common Files\Adobe\AdobeGCClient\locales'
@@ -49,3 +50,4 @@ Remove-Item "$LocalePath" -Force -Recurse
 
 $Shell = New-Object -ComObject "WScript.Shell"
 $Shell.Popup("Trial banner has been hidden!", 0, "Trial Banner Hider", 0)
+# $Button = $Shell.Popup("Trial banner has been hidden!", 0, "Trial Banner Hider", 0)
