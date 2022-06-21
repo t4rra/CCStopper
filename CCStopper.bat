411d1f6b8b8aa33721af5cd61d9fd8571fad0db7
Professional <shubhojeetde2@gmail.com>
2022-06-21 15:32:55 +0530

    Only elevate script if not already elevated

--- CCStopper.bat
+++ CCStopper.bat
@@ -4,8 +4,15 @@ set "Path=%Path%;%CD%;"
 mode con: cols=99 lines=35
 
 :: Asks for Administrator Permissions
-%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd","/c %~s0 ::","","runas",1)(window.close) && exit
+
+net session >nul 2>&1
+if %errorlevel% neq 0 goto elevate
+goto mainMenu
+
+:elevate
 cd /d "%~dp0"
+%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd","/c %~s0 ::","","runas",1)(window.close) && exit
+exit
 
 :: Main script
 :mainMenu
