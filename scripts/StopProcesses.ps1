2e40340eb0ca67e06e532123f9b35101597d1e14
Professional <shubhojeetde2@gmail.com>
2022-06-21 14:39:35 +0530

    Update ask for admin perms code block in powershell scripts

--- scripts/StopProcesses.ps1
+++ scripts/StopProcesses.ps1
@@ -3,14 +3,14 @@ $MyWindowsPrincipal = New-Object System.Security.Principal.WindowsPrincipal($MyW
 $AdminRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator
 
 if($MyWindowsPrincipal.IsInRole($AdminRole)) {
-$Host.UI.RawUI.WindowTitle = $MyInvocation.MyCommand.Definition + "(Elevated)"
-   Clear-Host
+	$Host.UI.RawUI.WindowTitle = $MyInvocation.MyCommand.Definition + "(Elevated)"
+	Clear-Host
 } else {
-   $NewProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell"
-   $NewProcess.Arguments = $MyInvocation.MyCommand.Definition
-   $NewProcess.Verb = "runas"
-   [System.Diagnostics.Process]::Start($NewProcess)
-   Exit
+	$NewProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell"
+	$NewProcess.Arguments = $MyInvocation.MyCommand.Definition
+	$NewProcess.Verb = "runas"
+	[System.Diagnostics.Process]::Start($NewProcess)
+	Exit
 }
 
 # Stops Adobe Processes and Services, source: https://gist.github.com/carcheky/530fd85ffff6719486038542a8b5b997#gistcomment-3586740
