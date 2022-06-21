65d797535832fc80a81eb8b6ecfe662378bbe07a
Professional <shubhojeetde2@gmail.com>
2022-06-21 21:00:50 +0530

    Move cd into script dir line in CCStopper.bat

--- CCStopper.bat
+++ CCStopper.bat
@@ -7,10 +7,10 @@ mode con: cols=99 lines=35
 
 net session >nul 2>&1
 if %errorlevel% neq 0 goto elevate
+cd /d "%~dp0"
 goto mainMenu
 
 :elevate
-cd /d "%~dp0"
 %1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd","/c %~s0 ::","","runas",1)(window.close) && exit
 exit
 
