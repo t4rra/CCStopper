37015e17c70eba9ad6ca05699d5fab0739fcf75d
Professional <shubhojeetde2@gmail.com>
2022-06-27 13:53:24 +0530

    Add label in ccstopper.bat

--- CCStopper.bat
+++ CCStopper.bat
@@ -6,14 +6,14 @@ mode con: cols=99 lines=35
 net session >nul 2>&1
 if %errorlevel% neq 0 goto elevate
 cd /d "%~dp0\scripts"
-goto mainMenu
-
 set "Path=%Path%;%CD%;"
+goto mainScript
 
 :elevate
 %1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd","/c %~s0 ::","","runas",1)(window.close)
 exit
 
+:mainScript
 :: Unblock files
 for %%a in (*.ps1) do (echo.>%%a:Zone.Identifier)
 
