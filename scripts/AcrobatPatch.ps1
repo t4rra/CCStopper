85b088e34bde13774e506bc6ebca1ba3f84d527c
Professional <63961221+ItsProfessional@users.noreply.github.com>
2022-07-24 14:19:38 +0530

    Add reversibility for AcrobatPatch module

--- scripts/AcrobatPatch.ps1
+++ scripts/AcrobatPatch.ps1
@@ -1,10 +1,20 @@
 Import-Module $PSScriptRoot\Functions.ps1
 Init -Title "Acrobat Patch"
 
-function EditReg {
+function Patch {
 	# Adds IsAMTEnforced with proper values, then deletes IsNGLEnfoced
 	Set-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Adobe\Adobe Acrobat\DC\Activation" -Name IsAMTEnforced -Value 1
 	Remove-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Adobe\Adobe Acrobat\DC\Activation" -Name IsNGLEnforced
+}
+
+function Revert {
+	# Adds IsNGLEnfoced with proper values, then deletes IsAMTEnforced
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Adobe\Adobe Acrobat\DC\Activation" -Name IsNGLEnforced -Value 1
+	Remove-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Adobe\Adobe Acrobat\DC\Activation" -Name IsAMTEnforced
+}
+
+function EditReg {
+	if ($Patched -eq $true) { Patch } else { Revert }
 	RestartAsk
 }
 
@@ -12,30 +22,21 @@ function RestartAsk {
 	ShowMenu -Back -Subtitles "AcrobatPatch Module" -Header "Acrobat patching complete!" -Description "The system needs to restart for changes to apply." -Options @(
 		@{
 			Name = "Restart now"
-			Code = {
-				Restart-Computer
-			}
+			Code = { Restart-Computer }
 		}
 	)
 }
 
-
 # Check if IsNGLEnforced already replaced w/ IsAMTEnforced
 $IsAMTEnforced = (Get-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Adobe\Adobe Acrobat\DC\Activation").IsAMTEnforced
 if ($IsAMTEnforced -eq 1) {
-	Clear-Host
-	Write-Output "Acrobat has already been patched."
-	Pause
-	Exit
+	$Patched = $true
+	ShowMenu -Back -Subtitles "AcrobatPatch Module" -Header "ACROBAT IS ALREADY PATCHED!" -Description "Would you like to revert the patch?" -Options @(
+		@{
+			Name = "Revert AcrobatPatch"
+			Code = { RegBackup -Msg "AcrobatPatch" }
+		}
+	)
 } else {
-	# Check if target path exists
-	$IsNGLEnforced = (Get-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Adobe\Adobe Acrobat\DC\Activation").IsNGLEnforced
-	if ($null -eq $IsNGLEnforced) {
-		Clear-Host
-		Write-Output "The target registry key cannot be found. Cannot proceed with Acrobat fix."
-		Pause
-		Exit
-	} else {
-		RegBackup -Msg "Acrobat Patch"
-	}
+	RegBackup -Msg "AcrobatPatch"
 }
