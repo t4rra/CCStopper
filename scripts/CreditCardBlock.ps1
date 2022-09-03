51357f6e20cde175c92e7ea62da71fdd5921357e
E-Soda <64815861+eaaasun@users.noreply.github.com>
2022-09-03 00:19:50 -0700

    new checking/files array system

--- scripts/CreditCardBlock.ps1
+++ scripts/CreditCardBlock.ps1
@@ -1,40 +1,51 @@
 Import-Module $PSScriptRoot\Functions.ps1
 Init -Title "Credit Card Prompt Block"
 
-$Files = @("${Env:ProgramFiles(x86)}\Common Files\Adobe\Adobe Desktop Common\ADS\Adobe Desktop Service.exe", "$Env:ProgramFiles\Common Files\Adobe\Adobe Desktop Common\NGL\adobe_licensing_wf.exe", "$Env:ProgramFiles\Common Files\Adobe\Adobxe Desktop Common\NGL\adobe_licensing_wf_helper.exe")
+$Files = @(
+	@{
+		Path  = "${Env:ProgramFiles(x86)}\Common Files\Adobe\Adobe Desktop Common\ADS\Adobe Desktop Service.exe"
+		Check = $False
+	},
+	@{ 
+		Path  = "$Env:ProgramFiles\Common Files\Adobe\Adobe Desktop Common\NGL\adobe_licensing_wf.exe"
+		Check = $False
+	},
+	@{
+		Path  = "$Env:ProgramFiles\Common Files\Adobe\Adobe Desktop Common\NGL\adobe_licensing_wf_helper.exe"
+		Check = $False
+	})
 
 # run checks for file
-function CheckFile($FileChecked) {
-	# check if file and corresponding firewall rule exist, if not return false
-	if (Test-Path $FileChecked -PathType Leaf) {
-		if (Get-NetFirewallRule -DisplayName $FirewallRuleName -ErrorAction SilentlyContinue) {
-			write-host "File $FileChecked exists and firewall rule exists"
-			pause
-			return $True
-		}
-		else {
-			write-host "File $FileChecked exists but firewall rule does not"
-			pause
-			return "file"
+foreach ($File in $Files) {
+	$FirewallRuleName = "CCStopper-InternetBlock_$($Files.IndexOf($File))"
+	if (Test-Path $File.Path) {
+		# file exists
+		if (Get-NetFirewallRule -DisplayName "KDE Connect" -ErrorAction SilentlyContinue) {
+			if ($?) {
+				# firewall rule exists
+				write-host "firewall rule exists"
+				# $File.Check = $True
+			} else {
+				# firewall rule does not exist, but file does
+				write-host "firewall rule does not exist"
+				# $File.Check = "file"
+			}
 		}
 	}
  else {
-		write-host "nothing exists lol"
-		pause
-		return $False
-	}	
+		# file does not exist
+		$File.Check = $False
+	}
 }
-
 
 function FirewallAction([switch]$Remove) {
 	foreach ($File in $Files) {
-		$FirewallRuleName = "CCStopper-InternetBlock_$($Files.IndexOf($File))"
-		if (CheckFile $File -and $Remove) {
+		if ($File.Check -and $Remove) {
 			# if file and firewall rule exist, and if remove flag is true, remove firewall rule
 			Remove-NetFirewallRule -DisplayName $FirewallRuleName
 			break
 		}
-		elseif (CheckFile $File) {
+		elseif ($File.Check) {
 			ShowMenu -Back -Subtitles "InternetBlock Module" -Header "Firewall Rules Already Set!" -Description "Would you like to remove all existing rules?" -Options @(
 				@{
 					Name = "Remove Rules"
@@ -44,13 +55,13 @@ function FirewallAction([switch]$Remove) {
 				}
 			)
 		}
-		elseif (CheckFile $File -eq "file") {
+		elseif ($FileStatus -eq "file") {
 			write-host "we back in da firewall action function about to add the rule"
 			pause
 			# if file exists but no firewall rule, create firewall rule
 			New-NetFirewallRule -DisplayName $FirewallRuleName -Direction Outbound -Program $File -Action Block
 		}
-		elseif (!(CheckFile $File)) {
+		elseif (!($FileStatus)) {
 			ShowMenu -Back -Subtitles "InternetBlock Module" -Header "Error! File not found!" -Description "Target files could not be found. If the trial prompts are still displayed, please open an issue on the Github repo."
 		}
 	}
