c21c24c5aad07433740400a829c1c0e68fc35bcd
E-Soda <64815861+eaaasun@users.noreply.github.com>
2022-09-03 17:15:27 -0700

    add end menu to creditcardblock

--- scripts/CreditCardBlock.ps1
+++ scripts/CreditCardBlock.ps1
@@ -3,15 +3,15 @@ Init -Title "Credit Card Prompt Block"
 
 $Files = @(
 	@{
-		Path  = "${Env:ProgramFiles(x86)}\Common Files\Adobe\Adobe Desktop Common\ADS\Adobe Desktop Service.exxe"
+		Path  = "${Env:ProgramFiles(x86)}\Common Files\Adobe\Adobe Desktop Common\ADS\Adobe Desktop Service.exe"
 		Check = $False
 	},
 	@{ 
-		Path  = "$Env:ProgramFiles\Common Files\Adobe\Adobe Desktop Common\NGL\adobe_licensing_wf.exxe"
+		Path  = "$Env:ProgramFiles\Common Files\Adobe\Adobe Desktop Common\NGL\adobe_licensing_wf.exe"
 		Check = $False
 	},
 	@{
-		Path  = "$Env:ProgramFiles\Common Files\Adobe\Adobe Desktop Common\NGL\adobe_licensing_wf_helper.exxe"
+		Path  = "$Env:ProgramFiles\Common Files\Adobe\Adobe Desktop Common\NGL\adobe_licensing_wf_helper.exe"
 		Check = $False
 	})
 
@@ -37,6 +37,7 @@ foreach ($File in $Files) {
 }
 
 function FirewallAction([switch]$Remove) {
+	$EndHeaderMSG = $Null
 	foreach ($File in $Files) {
 		$FirewallRuleName = "CCStopper-InternetBlock_$($Files.IndexOf($File))"
 		switch ($File.Check) {
@@ -44,6 +45,7 @@ function FirewallAction([switch]$Remove) {
 				if ($Remove) {
 					# if file and firewall rule exist, and if remove flag is true, remove firewall rule
 					Remove-NetFirewallRule -DisplayName $FirewallRuleName
+					$EndHeaderMSG = "Firewall rules removed!"
 				} else {
 					ShowMenu -Back -Subtitles "InternetBlock Module" -Header "Firewall Rules Already Set!" -Description "Would you like to remove all existing rules?" -Options @(
 						@{
@@ -58,6 +60,7 @@ function FirewallAction([switch]$Remove) {
 			"file" {
 				# if file exists but no firewall rule, create firewall rule
 				New-NetFirewallRule -DisplayName $FirewallRuleName -Direction Outbound -Program $File.Path -Action Block
+				$EndHeaderMSG = "Firewall rules created!"
 			}
 			$False {
 				ShowMenu -Back -Subtitles "InternetBlock Module" -Header "Error! File not found!" -Description "Target files could not be found. If the trial prompts are still displayed, please open an issue on the Github repo."
@@ -65,13 +68,7 @@ function FirewallAction([switch]$Remove) {
 		}
 	}
 	# end menus
-	write-host "the end"
-	Pause
-	exit
+	ShowMenu -Back -Subtitles "InternetBlock Module" -Header $EndHeaderMSG -Description "Operation has been completed successfully."
 }
 
 FirewallAction
-
-# ShowMenu -Back -Subtitles "InternetBlock Module" -Header $HeaderMSG
-
-# ShowMenu -Back -Subtitles "InternetBlock Module" if ($IsBlocked) { -Header "Unblocked internet!" } elseif ($IsNotBlocked) { -Header "Blocked internet!" }
