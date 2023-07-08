3954180338f7b1ad2317a7c46f077d0ff27a8fa5
E-Soda <64815861+eaaasun@users.noreply.github.com>
2023-07-07 19:01:39 -0700

    will it work??

--- runFromWeb/Installer.ps1
+++ runFromWeb/Installer.ps1
@@ -1,7 +1,8 @@
 $fileLocation = "$env:TEMP\CCStopper\Downloader.ps1"
+mkdir "$env:TEMP\CCStopper"
 
 irm "https://https://raw.githubusercontent.com/eaaasun/CCStopper/dev/runFromWeb/Downloader.ps1" | Out-File $fileLocation -Force
 
-Start-Process -FilePath "powershell.exe" -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$fileLocation`" -install" -Wait
+Start-Process -FilePath "powershell.exe" -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$fileLocation`" -install" -Verb runas -Wait
 Remove-Item -Path $fileLocation -Force
 Remove-Item -Path $MyInvocation.MyCommand.Path -Force
--- runFromWeb/Shortcut.ps1
+++ runFromWeb/Shortcut.ps1
@@ -1,7 +1,7 @@
 $fileLocation = "$env:TEMP\CCStopper\Downloader.ps1"
-
+mkdir "$env:TEMP\CCStopper"
 irm "https://https://raw.githubusercontent.com/eaaasun/CCStopper/dev/runFromWeb/Downloader.ps1" | Out-File $fileLocation -Force
 
-Start-Process -FilePath "powershell.exe" -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$fileLocation`" -shortcut" -Wait
+Start-Process -FilePath "powershell.exe" -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$fileLocation`" -shortcut" -Verb runas -Wait
 Remove-Item -Path $fileLocation -Force
 Remove-Item -Path $MyInvocation.MyCommand.Path -Force
