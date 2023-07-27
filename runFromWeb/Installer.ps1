f4533671bd0ea6f01c006d33e071b253fea42581
E-Soda <64815861+eaaasun@users.noreply.github.com>
2023-07-27 15:14:45 -0700

    remove runFromWeb

--- runFromWeb/Installer.ps1
+++
@@ -1,6 +0,0 @@
-$fileLocation = "$env:TEMP\CCStopperDownloader.ps1"
-
-Invoke-RestMethod "https://raw.githubusercontent.com/eaaasun/CCStopper/dev/runFromWeb/Downloader.ps1" -UseBasicParsing | Out-File $fileLocation -Force
-
-Start-Process -FilePath "powershell.exe" -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$fileLocation`" -install" -Verb runas -Wait
-Remove-Item -Path $fileLocation -Force
