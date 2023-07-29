2e7087f6f6ddc5699582a5da8cb8bfddaf84dfee
E-Soda <64815861+eaaasun@users.noreply.github.com>
2023-07-28 17:41:52 -0700

    remove makezip

--- makezip.ps1
+++
@@ -1,7 +0,0 @@
-Import-Module $PSScriptRoot\CCStopper_AIO.ps1
-$FolderName = "CCStopper_$Version"
-New-Item -ItemType Directory -Path ".\$FolderName"
-Copy-Item -Path ".\CCStopper.bat" -Destination ".\$FolderName\CCStopper.bat"
-Copy-Item -Path ".\CCStopper_AIO.ps1" -Destination ".\$FolderName\CCStopper_AIO.ps1"
-Compress-Archive -Path ".\$FolderName" -DestinationPath CCStopper.zip
-Remove-Item -Path ".\$FolderName" -Recurse
